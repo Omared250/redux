83a0db322604b69919af9e15919ff2421324639d
Omar <omared250@gmail.com>
2022-11-17 14:12:44 +0100

    counter slice

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  counter: 10,
 }
 
 export const counterSlice = createSlice({
@@ -13,18 +13,16 @@ export const counterSlice = createSlice({
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1
+      state.counter += 1;
     },
     decrement: (state) => {
-      state.value -= 1
+      state.counter -= 1;
     },
     incrementByAmount: (state, action) => {
-      state.value += action.payload
+      state.counter += action.payload;
     },
   },
 })
 
 // Action creators are generated for each case reducer function
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
-
-export default counterSlice.reducer
